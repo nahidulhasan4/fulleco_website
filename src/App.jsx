537fbec96de7ee3a15399aefe8281ec5425b6d24
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-10 10:09:56 -0700

    here and pretirrc with tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div className="text-blue-700 text-[60px] text-medium">App</div>
+    <div className="px-4 sm:px-8 py-2 sm:py-3 bg-sky-700 hover:bg-sky-800 text-blue-700 text-[60px] text-medium  ">App</div>
   )
 }
 
+
 export default App
+
+
