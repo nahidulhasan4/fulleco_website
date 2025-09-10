5bced3103a79e72b55f391b7281e85a35a54c4e0
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-10 12:35:22 -0700

    here finish

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,17 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import Home from "./pages/Home";
-import About from "./pages/About";
+// import About from "./pages/About";
 import Rootlayout from "./component/layout/Rootlayout";
 // import Contact from "./pages/Contact";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route>
-      <Route path="/" element={<Rootlayout />} />
-      <Route path="/about" element={<About />} />
+      <Route path="/" element={<Rootlayout />} >
+        <Route index element={<Home />} />
+      </Route>
+      
       
     </Route>,
   ),
--- src/component/layout/Rootlayout.jsx
+++ src/component/layout/Rootlayout.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 const Rootlayout = () => {
   return (
-    <div>Rootlayout</div>
+    <>
+    <Outlet />
+    </>
   )
 }
 
