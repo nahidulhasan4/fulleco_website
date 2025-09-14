c83cbbd2fb9ce805b88664cf86814132771481ce
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-14 10:57:35 -0700

    here complate rootlayout set up

--- src/component/layout/Rootlayout.jsx
+++ src/component/layout/Rootlayout.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
+import Header from '../Header'
+import Navber from '../Navber'
+import Footer from '../Footer'
 
 const Rootlayout = () => {
   return (
     <>
+    <Header/>
+    <Navber/>
     <Outlet />
+    <Footer/>
     </>
   )
 }
