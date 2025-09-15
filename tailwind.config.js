af3d9ef71c96ac9c14c0a017dc8e3e81f483e695
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-15 11:50:21 -0700

    here add some extra tailwind extension for get templete

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,23 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+const withMT = require("@material-tailwind/react/utils/withMT");
+
+module.exports = withMT({
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-       blackColor: '#000000',
-       whiteColor: '#FAFAFA'
-        },
+        blackColor: "#000000",
+        whiteColor: "#FAFAFA",
+      },
       fontFamily: {
-        poppins: ['Poppins', 'sans-serif'],
+        poppins: ["Poppins", "sans-serif"],
       },
       container: {
         center: true,
         screens: {
-          
           xl: "1170px",
-          
         },
       },
-      
     },
   },
   plugins: [],
-}
+});
