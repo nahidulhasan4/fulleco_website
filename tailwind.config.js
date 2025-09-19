6644fd615c0906adf78a9157c60c4febde0bc25b
nahidulhasan4 <nahidhasan4836@gmail.com>
2025-09-19 06:57:41 -0700

    here add some

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,13 @@
-// /** @type {import('tailwindcss').Config} */
 
-// /** @type {import('tailwindcss').Config} */
-// export default {
-//   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-//   theme: {
-//     extend: {
-//       colors: {
-//         blackColor: "#000000",
-//         whiteColor: "#FAFAFA",
-//       },
-//       fontFamily: {
-//         poppins: ["Poppins", "sans-serif"],
-//       },
-//       container: {
-//         center: true,
-//         screens: {
-//           xl: "1170px",
-//         },
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
+
+
 const withMT = require("@material-tailwind/react/utils/withMT");
  
 module.exports = withMT({
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
+  content: [
+    "./index.html", "./src/**/*.{js,ts,jsx,tsx}"
+  ],
+    theme: {
     extend: {
       colors: {
         blackColor: "#000000",
